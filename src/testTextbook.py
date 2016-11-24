49b3ee0223279c00f95f909f7d094348dc3fa398
Lucy <howobscene@msn.com>
2016-11-24 17:36:53 +0000

    I don't think I changed anything but I'm about to go and destroy my code.

--- src/testTextbook.py
+++ src/testTextbook.py
@@ -11,7 +11,6 @@ if __name__ == '__main__':
     model = cobra.test.create_test_model("textbook")
     include = {"EX_lac__D_e":models.FORWARD}
     exclude = []
-    #ignore = ["adp_c", "atp_c", "coa_c", "h2o_c", "h_c", "h_e", "nad_c", "nadh_c", "nadp_c", "nadph_c"]
     ignore = []
     
     fluxes = models.findEFM(model, include, exclude, ignore)
