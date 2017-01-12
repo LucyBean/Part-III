29f593abfcf93c1d2c025dd42000b1b87d591b2a
Lucy <howobscene@msn.com>
2017-01-12 17:48:25 +0000

    Refactored 'gen all' scripts into a FluxGenerator class.

---
+++ src/genAll.py
@@ -0,0 +1,166 @@
+'''
+Created on Jan 12, 2017
+
+@author: Lucy
+'''
+
+from src import models, helpers
+import time
+
+class FluxGenerator:
+    def __init__(self, model, startReaction, include, initialExclude):
+        self.model = model
+        self.reactions = model.reactions
+        self.reactionVals = helpers.genReactionVals(self.reactions)
+        self.startReaction = startReaction
+        self.include = include
+        self.exclude = initialExclude
+        self._useManual = False
+        self._verboseOutput = True
+        self._removeDuplicates = False
+        self._maxCount = -1
+        self._extra = lambda (flux, excludeVal): None
+        
+        # Output
+        self.efmsGenerated = [] # List of EFMs generated
+        self.infeasibleCount = 0 # Count of infeasible EFMs tried
+        self.duplicateCount = 0 # Count of duplicate EFMs generated
+        self.timeTaken = 0
+        
+    def useManualInput(self):
+        self._useManual = True
+        self._verboseOutput = True
+        print "q to quit"
+        print "e to view EFMs generated"
+        print "c to view counts"
+        
+    def setMaxCount(self, maxCount):
+        self._maxCount = maxCount
+        
+    def removeDuplicates(self):
+        self._removeDuplicates = True
+        
+    def setExtra(self, extra):
+        self._extra = extra
+        
+    def _reactionNamesToVal(self, reactionIDs):
+        return helpers.reactionNamesToVal(reactionIDs, self.reactionVals)
+    
+    def _findEFM(self, exclude):
+        return models.findEFM(self.model, self.include, exclude, 0)
+    
+    def _output(self, string):
+        if self._verboseOutput:
+            print string
+            
+    def printResults(self):
+        print "Generated:"
+        print "\t", len(self.efmsGenerated), "total EFMs"
+        print "\t", self.infeasibleCount, "infeasible EFMs tried"
+        print "\t", self.duplicateCount, "duplicate EFMs generated"
+        print "Time taken:", self.timeTaken
+                      
+
+
+    def genAll(self):
+        # Set up initial values
+        flux = self._findEFM(self.exclude)
+        
+        efmsToExplore = [] # List of EFMs left to check
+        
+        if flux is not None:
+            efmsToExplore.append((flux, self.exclude))
+            self.efmsGenerated.append(self._reactionNamesToVal(flux))
+            # Do anything extra that has been specified by the user
+            self._extra(flux, self._reactionNamesToVal(self.exclude))
+        
+        startTime = time.time()
+        waitTime = 0
+        userInput = ""
+        testedExclusions = [self._reactionNamesToVal(self.exclude)]
+        
+        while userInput != "q":
+            if efmsToExplore == []: # No EFMs left to explore
+                self._output("\nExplored all EFMs")
+                break
+            if self._maxCount > 0 and len(self.efmsGenerated) >= self._maxCount: # Reached max count
+                self._output("\nGenerated max number of EFMs")
+                break
+            
+            efm = efmsToExplore[-1] # Get the last element in the list
+            efmsToExplore = efmsToExplore[:-1]
+            flux = efm[0] # Extract the flux (list of reactions)
+            fluxVal = self._reactionNamesToVal(flux) # Convert list of reactions to val
+            exclude = efm[1] # Get the exclusion list that generated the flux
+            self._output("Exploring EFM " + str(fluxVal))
+            
+            # Find all possible knock outs
+            knockOutAble = [r for r in self.reactions if r.id not in self.include and r.id not in exclude]
+            # And try to knock out each reaction
+            for r in knockOutAble:
+                nexclude = exclude + [r.id]
+                nexcludeVal = self._reactionNamesToVal(nexclude)
+                self._output("Next exclusion set " + str(nexcludeVal))
+                
+                # Check for user input to pause or give feedback
+                if self._useManual:
+                    startWait = time.time()
+                    userInput = raw_input()
+                    if userInput == "q":
+                        break
+                    if userInput == "e":
+                        print self.efmsGenerated
+                    if userInput == "c":
+                        print "Generated", len(self.efmsGenerated)
+                        print "Generated", self.infeasibleCount, "infeasible EFMs"
+                        print "Generated", self.duplicateCount, "duplicate EFMs" 
+                    waitTime += time.time() - startWait
+                    
+                # Check if this exclusion set has been tested before
+                if nexcludeVal in testedExclusions:
+                    self._output("Duplicate exclusion set " + str(nexcludeVal))
+                    continue # Skip this exclusion set and try next
+                testedExclusions.append(nexcludeVal)
+                
+                # Generate the EFM
+                nflux = self._findEFM(nexclude)
+                
+                if nflux is None:
+                    # Infeasible model
+                    self.infeasibleCount += 1
+                    self._output("\tInfeasible")
+                else:
+                    # Feasible model
+                    efmReactions = nflux.keys()
+                    efmReactionsVal = self._reactionNamesToVal(efmReactions)
+                    self._output("\tFlux generated " + str(efmReactionsVal))
+                    
+                    # Check if this has been generated before
+                    if efmReactionsVal in self.efmsGenerated:
+                        self.duplicateCount += 1
+                    
+                    # Add this to the list of EFMs to check if:
+                        # not removing duplicates, or
+                        # it is not a duplicate
+                    if not self._removeDuplicates or efmReactionsVal not in self.efmsGenerated:
+                        self._output("\tFound flux " + str(efmReactionsVal))
+                        efmsToExplore.append((nflux, nexclude))
+                        self.efmsGenerated.append(efmReactionsVal)
+                        
+                    # Do anything extra that has been specified by the user
+                    self._extra(nflux, nexcludeVal)
+                    
+                    # Check whether max number has been generated
+                    if self._maxCount > 0 and len(self.efmsGenerated) >= self._maxCount:
+                        break
+                
+            endTime = time.time()
+            self.timeTaken = endTime - startTime - waitTime
+            self.printResults()
+            
+            
+            
+            
+            
+            
+            
--- src/textbookGenAllNoDuplicateRemoval.py
+++ src/textbookGenAllNoDuplicateRemoval.py
@@ -1,117 +1,20 @@
+from src.genAll import FluxGenerator
 import cobra
-from src import models, display, helpers
-import json
-import time
+from src import models
 
 model = cobra.io.load_json_model("toyModel.json")
-reactions = model.reactions
-startReaction = reactions.get_by_id("EX_G6P")
+startReaction = model.reactions.get_by_id("EX_G6P")
 include = {startReaction.id: models.FORWARD, "EX_Pyr": models.REVERSE}
-exclude = []
-flux = models.findEFM(model, include, exclude, 0)
-
-reactionVals = helpers.genReactionVals(reactions)
-
-def reactionNamesToVal(reactionIDs):
-    return helpers.reactionNamesToVal(reactionIDs, reactionVals)
-
-efmsToExplore = [(flux, exclude)]
-efmsGenerated = [reactionNamesToVal(flux.keys())]
-testedExclusions = [reactionNamesToVal(exclude)]
-displayOutput = {}
-displayOutput[0] = {}
-displayOutput[0]["desc"] = json.dumps("Include: " + str(include) + "<br/>Exclude: " + str(exclude))
-displayOutput[0]["fluxes"] = [flux]
-# This will keep track of which exclusion sets produced each EFM
-efmsGeneratedByExclusionSet = {}
-
-count = 0
-infeasibleCount = 0
-duplicateCount = 0
+initialExclude = []
+fg = FluxGenerator(model, startReaction, include, initialExclude)
 
 print "Use manual input? (y/n)",
-useManual = (raw_input() == "y")
-
+if (raw_input() == "y"):
+    fg.useManualInput()
+    
 print "Max number to generate: (enter -1 for no limit)",
 maxCount = int(raw_input())
-
-startTime = time.time()
-
-inp = ""
-
-while inp != "q":
-    if efmsToExplore == []:
-        print "\nExplored all EFMs"
-        break
-    if maxCount > 0 and len(efmsGenerated) > maxCount:
-        print "\nGenerated max number of EFMs"
-        break
-    print "Exploring EFM", count
-    efm = efmsToExplore[-1]
-    efmsToExplore = efmsToExplore[:-1]
-    flux = efm[0]
-    exclude = efm[1]
-    count += 1
+if maxCount > 0:
+    fg.setMaxCount(maxCount)
     
-    knockOutAble = [r for r in reactions if r.id not in include and r.id not in exclude]
-    for r in knockOutAble:
-        # Create a new exclusion set with this reaction and the exclusion set used to generate this
-        nexclude = exclude + [r.id]
-        nexclude.sort()
-        nexcludeVal = reactionNamesToVal(nexclude)
-        print "Next exclusion set", nexcludeVal
-        
-        if useManual:
-            inp = raw_input() # Pause to allow quitting or looking at input
-            if inp == "q":
-                break
-        
-        if nexcludeVal in testedExclusions: # See if this exclusion set has been tested
-            print "\tDuplicate exclusion set."
-            continue # Skip this exclusion set
-        
-        testedExclusions.append(nexcludeVal)
-        # Generate the EFM
-        nflux = models.findEFM(model, include, nexclude, 0)
-        
-        if nflux is not None:
-            # Find the value for this efm
-            efmReactions = nflux.keys()
-            efmReactionsVal = reactionNamesToVal(efmReactions)
-            print "\tFlux generated", efmReactionsVal
-            
-            efmsToExplore.append((nflux, nexclude))
-            efmsGenerated.append(efmReactionsVal)
-            # Check whether max number of EFMs have been generated
-            if maxCount > 0 and len(efmsGenerated) >= maxCount:
-                break
-            # See if this has been generated before and increment count if so
-            if efmReactionsVal not in efmsGeneratedByExclusionSet:
-                print "\tFound new", efmReactionsVal
-                efmsGeneratedByExclusionSet[efmReactionsVal] = []
-                # Save the display
-                i = efmReactionsVal
-                displayOutput[i] = {}
-                displayOutput[i]["desc"] = json.dumps("Include: " + str(include) + "<br/>Exclude: " + str(nexclude))
-                displayOutput[i]["fluxes"] = [nflux]
-            else:
-                duplicateCount += 1
-                print "\tEFM has been generated before"
-            efmsGeneratedByExclusionSet[efmReactionsVal].append(nexcludeVal)
-            
-        else:
-            infeasibleCount += 1
-            print "\tInfeasible"
-            
-# Optionally display results
-print "Enter r to display results",
-inp = raw_input()
-
-endTime = time.time()
-print "Generated", len(efmsGenerated), "in", (endTime - startTime), "s"
-print "Generated", infeasibleCount, "infeasible EFMs"
-print "Generated", duplicateCount, "duplicate EFMs"
-    
-if inp == "r":
-    display.displayAll(map_json="toyModelMap.json", toDisplay=displayOutput, title="All knockouts")
-            
+fg.genAll()
